196a0d7e65b141e112e4f5a0e76ffb8cd08e860a
tianbingyao22 <2242378609@qq.com>
2022-07-09 19:52:22 +0800

    附加练习1切换页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Home from "@/views/MyHome";
+import Type from "@/views/MyType";
+import Order from "@/views/MyOrder";
+import Mine from "@/views/MyMine";
 
 Vue.config.productionTip = false
 
+// 引入路由
+import VueRouter from "vue-router";
+// 使用路由插件
+Vue.use(VueRouter)
+// 创建路由规则数组
+const routes = [
+  { path: '/home', component: Home },
+  { path: '/type', component: Type },
+  { path: '/make', component: Order },
+  { path: '/mine', component: Mine },
+];
+const router=new VueRouter({
+  routes,
+});
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
