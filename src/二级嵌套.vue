2515d5245bace69158aeda00da86f7b3b2c42def
GuanSzzz <847388139@qq.com>
2022-07-09 21:14:09 +0800

    完成路由原理

---
+++ src/二级嵌套.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <router-link to="/home">首页</router-link> &nbsp;
+    <router-link to="/my">我得</router-link> &nbsp;
+    <router-link to="/tag">分类</router-link> &nbsp;
+    <router-link to="/order">我得订单</router-link> &nbsp;
+
+    <!-- 用原生的方法 -->
+    <!-- <span @click="btn('/home', 'Home')">首页</span> &nbsp;
+    <span @click="btn('/my', 'My')">我得</span> &nbsp;
+    <span @click="btn('/order', 'Order')">订单</span> &nbsp;
+    <span @click="btn('/tag', 'Tag')">分类</span> &nbsp; -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  // methods: {
+  //   btn(pathVal, nameVal) {
+  //     this.$router.push({
+  //       // path: pathVal,
+  //       name: nameVal,
+  // 传参
+  // query:{
+  //   title:'我是jqury原生跳转传参'
+  // },
+  // 原链接显示
+  // params: {
+  //   title: "我是params原生跳转传参",
+  // },
+  // 链接上不显示
+  //       });
+  //     },
+  //   },
+};
+</script>
+
+<style></style>
---
+++ src/路由原理.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>路由 - 工作原理手写</h1>
+    <a href="#/my">我得</a>&nbsp; <a href="#/order">订单</a>&nbsp;
+    <a href="#/tag">分类</a>&nbsp;
+    <component :is="comName"></component>
+  </div>
+</template>
+
+<script>
+import MyVue from "@/views/my.vue";
+import OrderVue from "@/views/order.vue";
+import TagVue from "@/views/tag.vue";
+export default {
+  data() {
+    return {
+      comName: "MyVue",
+    };
+  },
+  components: {
+    MyVue,
+    OrderVue,
+    TagVue,
+  },
+  created() {
+    // 监视window的hash变化
+    window.onhashchange = () => {
+      switch (location.hash) {
+        case "#/my":
+          this.comName = "MyVue";
+          break;
+        case "#/order":
+          this.comName = "OrderVue";
+          break;
+        case "#/tag":
+          this.comName = "TagVue";
+          break;
+      }
+    };
+  },
+};
+</script>
+
+<style></style>
